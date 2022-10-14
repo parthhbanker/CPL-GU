d0b4d2105d7e6add8881c9da615ab136044e9c9f
Het <hetparekh21@gmail.com>
2022-10-14 20:52:29 +0530

    category section

--- admin/ajax.php
+++ admin/ajax.php
@@ -8,21 +8,12 @@ if($action == 'login'){
 	if($login)
 		echo $login;
 }
-if($action == 'login2'){
-	$login = $crud->login2();
-	if($login)
-		echo $login;
-}
 if($action == 'logout'){
 	$logout = $crud->logout();
 	if($logout)
 		echo $logout;
 }
-if($action == 'logout2'){
-	$logout = $crud->logout2();
-	if($logout)
-		echo $logout;
-}
+
 if($action == 'save_user'){
 	$save = $crud->save_user();
 	if($save)
@@ -33,18 +24,8 @@ if($action == 'delete_user'){
 	if($save)
 		echo $save;
 }
-if($action == 'signup'){
-	$save = $crud->signup();
-	if($save)
-		echo $save;
-}
 if($action == 'update_account'){
 	$save = $crud->update_account();
-	if($save)
-		echo $save;
-}
-if($action == "save_settings"){
-	$save = $crud->save_settings();
 	if($save)
 		echo $save;
 }
@@ -79,52 +60,52 @@ if($action == "save_bid"){
 	if($save)
 		echo $save;
 }
-if($action == "delete_book"){
-	$save = $crud->delete_book();
-	if($save)
-		echo $save;
+// if($action == "delete_book"){
+// 	$save = $crud->delete_book();
+// 	if($save)
+// 		echo $save;
 
-}
+// }
 
-if($action == "get_booked_details"){
-	$save = $crud->get_booked_details();
-	if($save)
-		echo $save;
-}
-if($action == "save_movement"){
-	$save = $crud->save_movement();
-	if($save)
-		echo $save;
-}
-if($action == "delete_movement"){
-	$save = $crud->delete_movement();
-	if($save)
-		echo $save;
-}	
-if($action == "participate"){
-	$save = $crud->participate();
-	if($save)
-		echo $save;
-}
-if($action == "get_venue_report"){
-	$get = $crud->get_venue_report();
-	if($get)
-		echo $get;
-}
-if($action == "save_art_fs"){
-	$save = $crud->save_art_fs();
-	if($save)
-		echo $save;
-}
-if($action == "delete_art_fs"){
-	$save = $crud->delete_art_fs();
-	if($save)
-		echo $save;
-}
-if($action == "get_pdetails"){
-	$get = $crud->get_pdetails();
-	if($get)
-		echo $get;
-}
+// if($action == "get_booked_details"){
+// 	$save = $crud->get_booked_details();
+// 	if($save)
+// 		echo $save;
+// }
+// if($action == "save_movement"){
+// 	$save = $crud->save_movement();
+// 	if($save)
+// 		echo $save;
+// }
+// if($action == "delete_movement"){
+// 	$save = $crud->delete_movement();
+// 	if($save)
+// 		echo $save;
+// }	
+// if($action == "participate"){
+// 	$save = $crud->participate();
+// 	if($save)
+// 		echo $save;
+// }
+// if($action == "get_venue_report"){
+// 	$get = $crud->get_venue_report();
+// 	if($get)
+// 		echo $get;
+// }
+// if($action == "save_art_fs"){
+// 	$save = $crud->save_art_fs();
+// 	if($save)
+// 		echo $save;
+// }
+// if($action == "delete_art_fs"){
+// 	$save = $crud->delete_art_fs();
+// 	if($save)
+// 		echo $save;
+// }
+// if($action == "get_pdetails"){
+// 	$get = $crud->get_pdetails();
+// 	if($get)
+// 		echo $get;
+// }
 ob_end_flush();
 ?>
