6829d0c5e39e5a97d45f520b3d08e2fcd84d8383
Parth Banker <sw202119640002@gujaratuniversity.ac.in>
2022-10-15 16:56:52 +0530

    Team name on top bar

--- team/topbar.php
+++ team/topbar.php
@@ -9,15 +9,25 @@
 }
 </style>
 
+<!-- include db connect -->
+<?php include 'db_connect.php' ?>
+
 <nav class="navbar navbar-light fixed-top bg-primary" style="padding:0;min-height: 3.5rem">
   <div class="container-fluid mt-2 mb-2">
   	<div class="col-lg-12">
   		<div class="col-md-1 float-left" style="display: flex;">
   		
   		</div>
-      <!-- <div class="col-md-4 float-left text-white">
-        <large><b><?php// echo isset($_SESSION['system']['username']) ? $_SESSION['system']['name'] : '' ?></b></large>
-      </div> -->
+      <div class="col-md-4 float-left text-white">
+        <large><b><?php 
+          $team_id = $_SESSION['team_login_id'];
+
+          $team = mysqli_query($conn, "SELECT * FROM team WHERE team_Id = '$team_id'");
+          while($row=$team->fetch_assoc()):
+            echo "Team ".$row['team_name'];
+          endwhile;
+        ?></b></large>
+      </div> 
 	  	<div class="float-right">
         <div class=" dropdown mr-4">
             <a href="#" class="text-white dropdown-toggle"  id="account_settings" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo $_SESSION['team_login_username'] ?> </a>
